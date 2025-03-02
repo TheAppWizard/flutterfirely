08c3cdd10c29fd689a120701f8dc97784a3f89ba
TheAppWizard <theappwizard2408@gmail.com>
2025-03-02 19:57:36 +0530

    Firebase Options

--- lib/login/login_controller.dart
+++ lib/login/login_controller.dart
@@ -3,21 +3,35 @@ import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginController extends GetxController {
-  var googleSignIn = GoogleSignIn();
+  var googleSignIn = GoogleSignIn(
+    clientId: "1007988244157-342s5q9df6osb4rlgb6p2n9foqtqclgo.apps.googleusercontent.com",
+  );
+
   var googleAccount = Rx<GoogleSignInAccount?>(null);
 
-    Future<void> login() async {
-      googleAccount.value = await googleSignIn.signIn().then((value){
-        Get.to(const CrudView());
-      });
+  @override
+  void onInit() {
+    super.onInit();
+    print("LoginController initialized!");
+  }
+
+  /// Google Sign-In
+   login() async {
+    try {
+      final account = await googleSignIn.signIn();
+      if (account != null) {
+        googleAccount.value = account;
+        Get.to(() => const CrudView());
+      }
+    } catch (error,sta) {
+      print("Google Sign-In Error: $error $sta");
     }
+  }
 
-
-    logout() {
-      googleSignIn.signOut().then((value){
-        Get.back();
-      });
-    }
-
-
+  /// Google Sign-Out
+  logout() async {
+    await googleSignIn.signOut();
+    googleAccount.value = null;
+    Get.back();
+  }
 }
